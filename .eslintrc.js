0c0efc53e8d251c53c09ac17fc9b671fe6f1658c
Nicolas Martel <nicolas.martel@onespan.com>
2021-05-10 15:58:13 -0400

    chore: add linting/formatting

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+  },
+  extends: ['airbnb-base', 'prettier', 'plugin:prettier/recommended'],
+  parserOptions: {
+    ecmaVersion: 12,
+  },
+  rules: {},
+};
--- index.js
+++ index.js
@@ -1,33 +1,32 @@
-// In the main process.
-const { BrowserWindow, app } = require('electron')
+/* eslint-disable-next-line import/no-extraneous-dependencies */
+const { BrowserWindow, app } = require('electron');
 
 // Set a variable when the app is quitting.
 let isQuiting = false;
-let tray;
 let win;
 
 app.on('ready', () => {
-  win = new BrowserWindow()
+  win = new BrowserWindow();
 
   // Load ibroadcast web player
-  win.loadURL('https://media.ibroadcast.com')
+  win.loadURL('https://media.ibroadcast.com');
   // Maximize window
-  win.maximize()
+  win.maximize();
 
   // On close, minimize and don't quit
-  win.on('close', function (evt) {
+  win.on('close', (evt) => {
     if (!isQuiting) {
       evt.preventDefault();
-      win.hide()
+      win.hide();
     }
   });
 });
 
-app.on('before-quit', function (evt) {
+app.on('before-quit', () => {
   isQuiting = true;
 });
 
 // Clicking on dock shows the application
-app.on('activate', function (evt) {
-  win.show()
+app.on('activate', () => {
+  win.show();
 });
